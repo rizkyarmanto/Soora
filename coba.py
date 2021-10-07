1bb0a5430c7af2ac14cddb631ce1b37104b40846
rizkyarmanto <89382138+rizkyarmanto@users.noreply.github.com>
2021-10-07 14:27:49 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -9,6 +9,14 @@ DB_password = '1234'
 conn = psycopg2.connect(host=DB_host, dbname=DB_name, user=DB_user, password=DB_password)
 print(conn)
 cur = conn.cursor()
+#new_tabel = '''
+#    Create TABLE users(
+#        upload text        # Create Tabel 
+#    )
+#
+#'''
+#cur.execute(new_tabel)
+#conn.commit()
 f = open(r'C:\Users\ASUS\Documents\deepspeech\mic_vad_streaming\output.csv') #File path
 cur.copy_from(f, "users", sep=",")
 conn.commit()
